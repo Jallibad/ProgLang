8d57a5a1af37bbdad02ebb6bfd9e2b7c5b1105d7
Jordan <jallibad@gmail.com>
2017-12-01 19:53:59 -0500

    Finished HW3

--- hw3/NLParser.pl
+++ hw3/NLParser.pl
@@ -6,13 +6,55 @@ main :-
 	%Convert the lines in file to an list of sentences that are lists of words
 	lines_to_words(Lines, Words),
 	close(Str),
+	start(X, Y),
+	open('NL-parse-solution.txt', write, Output),
+	parse_sentences(X, Y, Words, Output),
+	close(Output).
 
-parse_sentences(X, Y, [Sentence|Sentences]) :-
+parse_sentences(_, _, [], _). % Base case, all sentences parsed
+parse_sentences(X, Y, [Sentence|Sentences], Output) :-
+	phrase(sentence, Sentence),
 	phrase((subject_phrase,verb,list(M)), Sentence),
-	object_phrase(M)
-parse_sentences(X, Y, [_|Sentences]) :-
-	writeln("Not a valid sentence"),
-	parse_sentences(X, Y, Sentences).
+	(
+		(
+			phrase(object_phrase,M),
+			button(X, Y, _),
+			writeln(Output, "Valid move"),
+			parse_sentences(X, Y, Sentences, Output)
+		);
+		(
+			phrase(direction_object_phrase,M),
+			phrase((list([A|[]]),object,list([D|[]])), M),
+			number_codes(N, A),
+			move(X, Y, NX, NY, N, D),
+			writeln(Output, "Valid move"),
+			parse_sentences(NX, NY, Sentences, Output)
+		);
+		writeln(Output, "Not a valid move")
+	).
+parse_sentences(X, Y, [Sentence|Sentences], Output) :-
+	\+ phrase(sentence, Sentence),
+	writeln(Output, "Not a valid sentence"),
+	parse_sentences(X, Y, Sentences, Output).
+
+translate(X, Y, X, NY, "up") :-
+	NY is Y-1.
+translate(X, Y, X, NY, "down") :-
+	NY is Y+1.
+translate(X, Y, NX, Y, "left") :-
+	NX is X-1.
+translate(X, Y, NX, Y, "right") :-
+	NX is X+1.
+
+move(X, Y, X, Y, 0, _).
+move(X, Y, NX, NY, N, D) :-
+	translate(X, Y, PX, PY, D),
+	\+ wall(PX, PY),
+	info(W, H, _),
+	0 =< PX, PX < W, % Check bounds
+	0 =< PY, PY < H,
+	NN is N-1,
+	move(PX, PY, NX, NY, NN, D).
 
 % Credit to StackOverflow and author Ishq for file parser
 % https://stackoverflow.com/a/4805931
--- hw3/mazeSolver.pl
+++ hw3/mazeSolver.pl
@@ -1,38 +1,49 @@
 :- use_module(mazeInfo, [info/3, wall/2, button/3, num_buttons/1, start/2, goal/2]).
 
-solve(_, X, Y, X, Y, Visited, [[X,Y]|Visited]).
-solve(CanButton, X, Y, GX, GY, Visited, Solution) :-
-	maplist(dif([X,Y]), Visited),
+% This predicate solves the maze for arbitrary start and end points
+% First argument is whether einstein can press any buttons (for type c)
+% Second and third arguments are the starting location
+% Fourth and fifth are the goal location
+% Sixth is the list of visited locations to avoid infinite loops
+% Seventh is the answer
+solve(_, X, Y, X, Y, Visited, [[X,Y]|Visited]). % Base case
+solve(CanButton, X, Y, GX, GY, Visited, Solution) :- % Recursive search
+	maplist(dif([X,Y]), Visited), % Have we already passed here?
 	info(W, H, _),
-	0 =< X, X < W,
+	0 =< X, X < W, % Check bounds
 	0 =< Y, Y < H,
-	(
+	( % Avoid buttons if need be
 		CanButton;
 		\+ button(X, Y, _)
 	),
-	\+ wall(X,Y),
-	X1 is X-1,
+	\+ wall(X,Y), % Avoid walls
+	X1 is X-1, % REEEEE
 	X2 is X+1,
 	Y1 is Y-1,
 	Y2 is Y+1,
 	NVisited = [[X,Y]|Visited],
 	(
-		solve(CanButton, X1, Y, GX, GY, NVisited, Solution);
-		solve(CanButton, X, Y1, GX, GY, NVisited, Solution);
-		solve(CanButton, X2, Y, GX, GY, NVisited, Solution);
-		solve(CanButton, X, Y2, GX, GY, NVisited, Solution)
+		solve(CanButton, X1, Y, GX, GY, NVisited, Solution); % Left
+		solve(CanButton, X, Y1, GX, GY, NVisited, Solution); % Up
+		solve(CanButton, X2, Y, GX, GY, NVisited, Solution); % Right
+		solve(CanButton, X, Y2, GX, GY, NVisited, Solution)  % Down
 	).
 
-solveButton(X, Y, ButtonID, Path) :-
-	num_buttons(ButtonID),
+% This predicate solves the maze touching each button in order
+% First two arguments are start location
+% Third is the next button that should be pressed
+% Fourth is the answer
+solveButton(X, Y, ButtonID, Path) :- % Base case
+	num_buttons(ButtonID), % If we've pressed every button
 	goal(GX, GY),
+	% Path directly to the goal, we can ignore buttons now
 	solve(true, X, Y, GX, GY, [], Path).
-solveButton(X, Y, ButtonID, Path) :-
-	button(GX, GY, ButtonID),
+solveButton(X, Y, ButtonID, Path) :- % Recusively path to each button
+	button(GX, GY, ButtonID), % Get the location of the current button
 	NButtonID is ButtonID+1,
-	solveButton(GX, GY, NButtonID, PartialPath1),
-	solve(false, X, Y, GX, GY, [], [_|PartialPath2]),
-	append(PartialPath1, PartialPath2, Path).
+	solveButton(GX, GY, NButtonID, PartialPath1), % Recurse from button
+	solve(false, X, Y, GX, GY, [], [_|PartialPath2]), % Path to button
+	append(PartialPath1, PartialPath2, Path). % Combine the two for the answer
 
 main :-
 	start(X,Y),
@@ -40,13 +51,13 @@ main :-
 	open("path-solution.txt", write, File),
 	(
 		(
-			info(_,_,a),
+			info(_,_,a), % a type case
 			solve(true, X, Y, GX, GY, [], BSolution),
 			reverse(BSolution, Solution),
 			maplist(writeln(File), Solution)
 		);
 		(
-			info(_,_,_),
+			info(_,_,_), % Otherwise handle b and c the same
 			solveButton(X, Y, 1, BSolution),
 			reverse(BSolution, Solution),
 			maplist(writeln(File), Solution)
